d855c14169445ba82b58072f716097a0e4dd3b7b
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-04-29 20:11:58 +0000

    minor changes to build process

--- dev_things.sh
+++ dev_things.sh
@@ -3,5 +3,7 @@ prep_conda() {
     pip install gdown
     gdown 1xtqmAVYeISAmicPC0QA753MZ8Z4M2ttA
     mv conda.zip /workspaces/project/chipyard/
+    
+    if [[ -d /workspaces/project/chipyard/.conda-env ]]; then return 0; fi
     cd /workspaces/project/chipyard/ && unzip conda.zip
 }
